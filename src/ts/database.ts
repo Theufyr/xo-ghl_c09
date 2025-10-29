35e3d78eeba94219b8cc3bd4ca04dac5343bff04
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2025-10-29 16:46:29 +0100

    feat: test database

---
+++ src/ts/database.ts
@@ -0,0 +1,10 @@
+const arcaneMajor: { name: string, question: string, answer1: string, answer2: string, answer3: string, answer4: string, ok: number } = {
+        name: "Le Fou",
+        question: "Le Fou est-il un symbole de :",
+        answer1: "Prudence",
+        answer2: "Planification",
+        answer3: "Réflexion",
+        answer4: "Spontanéité",
+        ok: 4
+};
+export default arcaneMajor;
---
+++ src/ts/main.ts
@@ -0,0 +1,2 @@
+import arcaneMajor from "./database.js";
+console.log(arcaneMajor.name);
