98266a9b8aaaeef1c1eb43aeb88f899fb954eab0
Theufyr Huhwhat <204263008+Theufyr@users.noreply.github.com>
2025-11-04 16:23:12 +0100

    chore : update database, scores

--- src/ts/storage.ts
+++ src/ts/storage.ts
@@ -1,15 +1,16 @@
 // on prépare la sauvegarde
-function setStorage(dSelect: number[], sCard: number, uScore: number): void {
+function setStorage(dSelect: number[], sCard: number, uScore: number, bScore: number): void {
     let storageInfos = {
         drawSelection: dSelect,
         selectedCard: sCard,
-        userScore: uScore
+        userScore: uScore,
+        bestScore: bScore
     }
     // on sérialise et on sauvegarde
     localStorage.setItem("storageInfos", JSON.stringify(storageInfos));
 }
 // on prépare la sauvegarde
-function getStorage(): {drawSelection: number[], selectedCard: number, userScore: number} | null {
+function getStorage(): {drawSelection: number[], selectedCard: number, userScore: number, bestScore: number} | null {
     let storageInfos: any = localStorage.getItem("storageInfos");
     // on vérifie qu'il y a une sauvegarde
     if (storageInfos !== null) {
